81afa2250d6a32d6789ca88c6543cc310193abee
Brian Park <brian@xparks.net>
2021-03-19 11:08:02 -0700

    src/pstrings.h: Fix docstring for strcmp_PP(), any string is *greater than* nullptr

--- src/pstrings/pstrings.h
+++ src/pstrings/pstrings.h
@@ -42,8 +42,8 @@ namespace ace_common {
  * Compare 2 strings which are both stored in PROGMEM. This is similar to the
  * `strcmp_P(a, p)` except that both strings are in PROGMEM instead of just the
  * second string. Either `a` or `b` can be a `nullptr`. Two `nullptr` are
- * compared to be equal to each other. Any string is assumed to be "less than"
- * a `nullptr`.
+ * compared to be equal to each other. Any non-null string will be considered to
+ * be greater than a `nullptr`.
  */
 int strcmp_PP(const char* a, const char* b);
 
